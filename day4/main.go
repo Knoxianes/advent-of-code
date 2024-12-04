5be91f4fa316c06698abdd144064909bfbd7e0ce
Knoxianes <strahinja2001@gmail.com>
2024-12-04 11:39:03 +0100

    done day4

--- day4/main.go
+++ day4/main.go
@@ -30,47 +30,15 @@ func main() {
 
 	words := findWords()
 	fmt.Println(len(words))
-	fmt.Println(calculateAnswer(words))
-}
-
-func calculateAnswer(words []Word) int {
-	var tmpWords []Word
-	for _, word := range words {
-		if !isCoridnatesInList(word.Cordinates, tmpWords) {
-			tmpWords = append(tmpWords, word)
-		}
-	}
-
-	return len(tmpWords)
-}
-
-func isCoridnatesInList(coridnates []Cordinate, words []Word) bool {
-	ret := false
-	for _, word := range words {
-		numberOfExists := 0
-		for _, coridnate := range coridnates {
-			exists := false
-			for _, wordCoridnate := range word.Cordinates {
-				if coridnate.x == wordCoridnate.x && coridnate.y == wordCoridnate.y {
-					exists = true
-					break
-				}
-			}
-			if exists {
-				numberOfExists++
-			}
-		}
-		if numberOfExists == 4 {
-			return true
-		}
-	}
-	return ret
 }
 
 func findWords() []Word {
 	var words []Word
-	for i := 0; i < len(maze); i++ {
-		for j := 0; j < len(maze[i]); j++ {
+	for i := 1; i < len(maze)-1; i++ {
+		for j := 1; j < len(maze[i])-1; j++ {
+			if maze[i][j] != 'A' {
+				continue
+			}
 			traverse(i, j, &words)
 		}
 	}
@@ -78,113 +46,23 @@ func findWords() []Word {
 }
 
 func traverse(i, j int, words *[]Word) {
-	maxLeft := j - 3
-	maxRight := j + 3
-	maxUp := i - 3
-	maxDown := i + 3
+	topLeftChar := maze[i-1][j-1]
+	topRightChar := maze[i-1][j+1]
+	bottomLeftChar := maze[i+1][j-1]
+	bottomRightChar := maze[i+1][j+1]
 
-	if maxLeft >= 0 {
-		var word Word
-		var cordinates []Cordinate
-		runes := []rune{}
-		for k := j; k >= maxLeft; k-- {
-			runes = append(runes, maze[i][k])
-			cordinates = append(cordinates, Cordinate{x: k, y: i})
-		}
-		word.word = string(runes)
-		word.Cordinates = cordinates
-		*words = append(*words, word)
+	mainDiagonal := false
+	otherDiagonal := false
+
+	if (topLeftChar == 'M' && bottomRightChar == 'S') || (topLeftChar == 'S' && bottomRightChar == 'M') {
+		mainDiagonal = true
 	}
 
-	if maxRight < len(maze[i]) {
-		var word Word
-		var cordinates []Cordinate
-		runes := []rune{}
-		for k := j; k <= maxRight; k++ {
-			runes = append(runes, maze[i][k])
-			cordinates = append(cordinates, Cordinate{x: k, y: i})
-		}
-		word.word = string(runes)
-		word.Cordinates = cordinates
-		*words = append(*words, word)
+	if (topRightChar == 'M' && bottomLeftChar == 'S') || (topRightChar == 'S' && bottomLeftChar == 'M') {
+		otherDiagonal = true
 	}
 
-	if maxUp >= 0 {
-		var word Word
-		var cordinates []Cordinate
-		runes := []rune{}
-		for k := i; k >= maxUp; k-- {
-			runes = append(runes, maze[k][j])
-			cordinates = append(cordinates, Cordinate{x: j, y: k})
-		}
-		word.word = string(runes)
-		word.Cordinates = cordinates
-		*words = append(*words, word)
-	}
-
-	if maxDown < len(maze) {
-		var word Word
-		var cordinates []Cordinate
-		runes := []rune{}
-		for k := i; k <= maxDown; k++ {
-			runes = append(runes, maze[k][j])
-			cordinates = append(cordinates, Cordinate{x: j, y: k})
-		}
-		word.word = string(runes)
-		word.Cordinates = cordinates
-		*words = append(*words, word)
-	}
-
-	if maxLeft >= 0 && maxUp >= 0 {
-		var word Word
-		var cordinates []Cordinate
-		runes := []rune{}
-		for k := 0; k <= 3; k++ {
-			runes = append(runes, maze[i-k][j-k])
-			cordinates = append(cordinates, Cordinate{x: j - k, y: i - k})
-		}
-		word.word = string(runes)
-		word.Cordinates = cordinates
-		*words = append(*words, word)
-	}
-
-	if maxLeft >= 0 && maxDown < len(maze) {
-		var word Word
-		var cordinates []Cordinate
-		runes := []rune{}
-		for k := 0; k <= 3; k++ {
-			runes = append(runes, maze[i+k][j-k])
-			cordinates = append(cordinates, Cordinate{x: j - k, y: i + k})
-		}
-		word.word = string(runes)
-		word.Cordinates = cordinates
-		*words = append(*words, word)
-	}
-
-	if maxRight < len(maze[i]) && maxUp >= 0 {
-		var word Word
-		var cordinates []Cordinate
-		runes := []rune{}
-		for k := 0; k <= 3; k++ {
-			runes = append(runes, maze[i-k][j+k])
-			cordinates = append(cordinates, Cordinate{x: j + k, y: i - k})
-		}
-		word.word = string(runes)
-		word.Cordinates = cordinates
-		*words = append(*words, word)
-
-	}
-
-	if maxRight < len(maze[i]) && maxDown < len(maze) {
-		var word Word
-		var cordinates []Cordinate
-		runes := []rune{}
-		for k := 0; k <= 3; k++ {
-			runes = append(runes, maze[i+k][j+k])
-			cordinates = append(cordinates, Cordinate{x: j + k, y: i + k})
-		}
-		word.word = string(runes)
-		word.Cordinates = cordinates
-		*words = append(*words, word)
+	if mainDiagonal && otherDiagonal {
+		*words = append(*words, Word{})
 	}
 }
